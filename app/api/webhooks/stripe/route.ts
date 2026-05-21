7ae78b39662f129d544c0a66907c6253a0bf385f
Willywld <willy.wlody@fr.esmod.net>
2026-05-21 13:34:31 +0000

    modif

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,5 +1,6 @@
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
+
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
@@ -10,6 +11,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
 
 export async function POST(req: Request) {
+  console.log("WEBHOOK HIT");
   const body = await req.text();
   const signature = req.headers.get("stripe-signature");
 
